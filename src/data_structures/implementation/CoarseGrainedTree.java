3a931782b8eb3ea77aafc4351817351ba2b54740
Mauro <mauropalsgraaf@hotmail.com>
2016-11-12 15:27:40 +0100

    Implement Add method in coarseGrainedTree

--- src/data_structures/implementation/CoarseGrainedTree.java
+++ src/data_structures/implementation/CoarseGrainedTree.java
@@ -7,17 +7,25 @@ import data_structures.implementation.coarsegrainedtree.Node;
 
 public class CoarseGrainedTree<T extends Comparable<T>> implements Sorted<T> {
 
-    private Node<T> left;
-    private Node<T> right;
+    private Node<T> top;
 
-    public void add(T t) {
+    public void add(T value) {
+        Node<T> nodeToAdd = new Node<>(value);
+
+        if (top == null) {
+            top = nodeToAdd;
+
+            return;
+        }
+
+        top.add(nodeToAdd);
     }
 
-    public void remove(T t) {
+    public void remove(T value) {
         throw new UnsupportedOperationException();
     }
 
     public ArrayList<T> toArrayList() {
-        throw new UnsupportedOperationException();
+        
     }
 }
