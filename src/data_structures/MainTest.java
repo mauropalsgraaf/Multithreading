6d127e2db1f1553969a76b86d02c4748c9aa53f4
Mauro <mauropalsgraaf@hotmail.com>
2016-11-12 13:52:58 +0100

    Add initial version of multithreading course

---
+++ src/data_structures/MainTest.java
@@ -0,0 +1,27 @@
+package data_structures;
+
+import data_structures.implementation.CoarseGrainedList;
+
+import java.util.ArrayList;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+        CoarseGrainedList<Integer> list = new CoarseGrainedList<>();
+
+        list.add(4);
+        list.add(1);
+        list.add(2);
+        list.add(5);
+        list.add(3);
+        list.add(3);
+
+        ArrayList<Integer> list1 = list.toArrayList();
+        System.out.println("list1 = " + list1);
+
+        list.remove(3);
+
+        list1 = list.toArrayList();
+        System.out.println("list1 = " + list1);
+    }
+}
---
+++ src/data_structures/implementation/CoarseGrainedList.java
@@ -0,0 +1,88 @@
+package data_structures.implementation;
+
+import java.util.ArrayList;
+
+import data_structures.Sorted;
+import data_structures.implementation.coarsegrainedlist.Node;
+
+public class CoarseGrainedList<T extends Comparable<T>> implements Sorted<T> {
+
+    private Node<T> head;
+
+    public void add(T value) {
+        Node<T> nodeToAdd = new Node<>(value);
+
+        if (head == null) { //if list is empty
+            head = nodeToAdd;
+
+            return;
+        }
+
+        if (value.compareTo(head.getValue()) <= 0) { //if item to add is smaller or equal to head, just add it in front
+            nodeToAdd.setNext(head);
+            head = nodeToAdd;
+
+            return;
+        }
+
+        Node<T> previous = head;
+        Node<T> nodeToCompare = head.getNext();
+
+        while (nodeToCompare != null) {
+
+            if (value.compareTo(nodeToCompare.getValue()) <= 0) {
+                previous.setNext(nodeToAdd);
+                nodeToAdd.setNext(nodeToCompare);
+
+                return;
+            }
+
+            previous = nodeToCompare;
+            nodeToCompare = nodeToCompare.getNext();
+        }
+
+        previous.setNext(nodeToAdd);
+    }
+
+    public void remove(T value) {
+        while (head.getValue().compareTo(value) == 0) {
+            head = head.getNext();
+        }
+
+        Node<T> previous = head;
+        Node<T> nodeToCompare = head.getNext();
+
+        while (nodeToCompare != null) {
+            int nodeComparator = value.compareTo(nodeToCompare.getValue());
+            if (nodeComparator == 0) {
+                previous.setNext(nodeToCompare.getNext());
+            }
+
+            if (nodeComparator < 0) { //The items are smaller at this point, so we can omit the rest
+                return;
+            }
+
+            previous = previous.getNext();
+            nodeToCompare = nodeToCompare.getNext();
+        }
+    }
+
+    public ArrayList<T> toArrayList() {
+        ArrayList<T> list = new ArrayList<>();
+        if (head == null) {
+            return list;
+        }
+
+        list.add(head.getValue());
+
+        Node<T> nodeToCompare = head.getNext();
+
+        while (nodeToCompare != null) {
+            list.add(nodeToCompare.getValue());
+
+            nodeToCompare = nodeToCompare.getNext();
+        }
+
+        return list;
+    }
+}
